d4314d3dc2f3589694c552256fe7b5cb65842574
Zhu Ye Deng <180813277+d-zhuye@users.noreply.github.com>
2025-04-24 16:05:39 -0700

    fix: Decrease Montserrat font-weight to 700
    The previous font weight (900) was too bold and did not accurate reflect
    the design mockup.

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,14 +2,13 @@ import { Montserrat_Alternates, Albert_Sans } from "next/font/google";
 
 export const montserrat = Montserrat_Alternates({
   subsets: ["latin"],
-  weight: ["400"],
+  weight: "700",
   variable: "--font-montserrat",
   display: "swap",
 });
 
 export const albertSans = Albert_Sans({
   subsets: ["latin"],
-  weight: ["400"],
   variable: "--font-albert-sans",
   display: "swap",
 });
