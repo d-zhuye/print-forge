7f94aedcf1db8b5a39eb370173e300600d62477d
Zhu Ye Deng <180813277+d-zhuye@users.noreply.github.com>
2025-04-24 17:04:56 -0700

    feat: Add models details page

--- app/3d-models/[slug]/page.tsx
+++ app/3d-models/[slug]/page.tsx
@@ -1,16 +1,47 @@
 import { getAllModels } from "@/app/lib/models";
 import { Model } from "@/app/types/types";
+import Link from "next/link";
+import Image from "next/image";
+import heroImage from "@/public/hero-image.png";
+import { FaRegHeart } from "react-icons/fa";
 
-export default async function ModelDetails({ params }) {
+type Params = {
+  state: string;
+};
+
+export default async function ModelDetails({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
   const { slug } = await params;
-  console.log(slug);
 
   const data = await getAllModels();
 
-  const product: Model | undefined = data.find((product: Model) => {
+  const model: Model | undefined = data.find((product: Model) => {
     const skeweredName = product.name.toLowerCase().split(" ").join("-");
     return skeweredName == slug;
   });
-  console.log(product);
-  return <h1>Model Details</h1>;
+
+  if (!model) throw new Error();
+
+  return (
+    <main>
+      <Link href="/3d-models" className="block py-4 uppercase text-center">
+        Back to overview
+      </Link>
+      <section className="flex flex-col justify-start items-start gap-4">
+        <Image src={heroImage} alt={model.name} />
+        <div className="text-2xl flex items-center gap-1 mt-auto">
+          <FaRegHeart className="hover:fill-red-700 hover:cursor-pointer" />
+          <p>{model.likes}</p>
+        </div>
+        <h2>{model.name}</h2>
+        <span className="px-3 border-[1px] border-neutral-400/60 rounded-2xl relative">
+          {model.category}
+        </span>
+        <p>{model.description}</p>
+      </section>
+    </main>
+  );
 }
